6886ce1c619728de06fca5c2055d4b29700474c9
Mike L <firyace@gmail.com>
2021-03-16 09:45:24 -0600

    Update settings.py

--- SocialClubLoaner/settings.py
+++ SocialClubLoaner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '07cg)j!^#$4mw^ir-%yes(gtw-6_cvvsq+v!g1gw%k^*%6)$00'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
